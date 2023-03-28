170e9305b3e9b15ce1c6632a9e641d985543a49f
Bobby Luly <bobbyluly@gmail.com>
2023-03-27 19:34:26 -0600

    Adjust validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,6 @@
 class Item < ApplicationRecord
-  validates_presence_of :name,
-                        :description,
-                        :unit_price,
-                        :merchant_id
+  validates :name, presence: true
+  validates :unit_price, presence: true, numericality: true
 
   belongs_to :merchant
   has_many :invoice_items
