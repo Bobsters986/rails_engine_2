a586cddc07800b102cfb27e75099ed7ced2ff0d6
Bobby Luly <bobbyluly@gmail.com>
2023-03-28 11:10:31 -0600

    Test: create tests for finding one item endpoint
    Create happy/sad path, and edgecase tests

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -28,4 +28,48 @@ RSpec.describe "Items API", type: :request do
       end
     end
   end
+
+  context "#show" do
+    before do
+      @first_item = Item.first
+      get "/api/v1/items/#{@first_item.id}"
+    end
+
+    context "when successful" do
+      it "returns one item" do
+        expect(response).to be_successful
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed[:data].keys).to eq([:id, :type, :attributes])
+        expect(parsed[:data][:attributes].keys).to eq([:name, :description, :unit_price, :merchant_id])
+        expect(parsed[:data][:id]).to eq(@first_item.id.to_s)
+        expect(parsed[:data][:type]).to eq('item')
+        expect(parsed[:data][:attributes][:name]).to eq(@first_item.name)
+        expect(parsed[:data][:attributes][:description]).to eq(@first_item.description)
+        expect(parsed[:data][:attributes][:unit_price]).to eq(@first_item.unit_price)
+        expect(parsed[:data][:attributes][:merchant_id]).to eq(@first_item.merchant_id)
+      end
+    end
+
+    context "when UNsuccessful" do
+      it "returns an error message for invalid id" do
+        get "/api/v1/items/986986"
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(404)
+        expect(parsed[:error]).to eq("Couldn't find Item with 'id'=986986")
+      end
+
+      it "returns an error message for string instead of integer" do
+        get "/api/v1/items/hello"
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(404)
+        expect(parsed[:error]).to eq("Couldn't find Item with 'id'=hello")
+      end
+    end
+  end
 end
