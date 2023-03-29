c8fe031f8077d98461811b418fa94f86616e6482
Bobby Luly <bobbyluly@gmail.com>
2023-03-29 12:07:27 -0600

    Add validation for presence of item description

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   validates :name, presence: true
+  validates :description, presence: true
   validates :unit_price, presence: true, numericality: true
   validates :merchant_id, presence: true
   # validate :merchant_id_exists
