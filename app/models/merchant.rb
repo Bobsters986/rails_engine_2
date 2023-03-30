517d3d17bce09ac2c9e8ea9783879e0d55ed40e7
Bobby Luly <bobbyluly@gmail.com>
2023-03-30 13:42:37 -0600

    Create .find_by_name model method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,10 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :items
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.find_by_name(criteria)
+    where("name ILIKE ?", "%#{criteria}%")
+    .order(:name)
+    .first
+  end
 end
