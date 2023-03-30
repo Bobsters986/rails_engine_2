513114be7b022f5aa9c891ceb02fef0ff6056c4f
Bobby Luly <bobbyluly@gmail.com>
2023-03-30 13:38:57 -0600

    Feat: create .find_by_name merchant class method
    - Finds a merchant by search, criteria case insensitive
    - - alphabetical order, grabs first result in list

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -13,4 +13,28 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:customers).through(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
   end
+
+  describe "class methods" do
+    describe ".find_by_name" do
+      let!(:merchant_1) { create(:merchant, name: "Turing") }
+      let!(:merchant_2) { create(:merchant, name: "Ring World") }
+      let!(:merchant_3) { create(:merchant, name: "One Ring to Rule them All") }
+
+      context "it is case insensitive, in alphabetical order, grabs first result" do
+        it "finds a merchant by search criteria" do
+          searched_merchant = Merchant.find_by_name("ring")
+
+          expect(searched_merchant).to eq(merchant_3)
+          expect(searched_merchant).to_not eq(merchant_2)
+          expect(searched_merchant).to_not eq(merchant_1)
+        end
+      end
+
+      it "returns nil if no merchant is found" do
+        searched_merchant = Merchant.find_by_name("wrong")
+        
+        expect(searched_merchant).to eq(nil)
+      end
+    end
+  end
 end
